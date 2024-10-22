34542bb5364af5c93c3d0292a8530e1c0cd3c782
nefintosik <93140131+nefintosik@users.noreply.github.com>
2024-10-22 20:18:36 +0700

    Update app.py
    Теперь бот может писать в группу

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import os
-
 from dotenv import load_dotenv
 import asyncio
-import sqlite3
+import aiosqlite
 from aiogram import Bot, Dispatcher
 from aiogram.types import Message
 from quart import Quart, jsonify, render_template, request
@@ -10,63 +9,47 @@ from db import init_db
 
 load_dotenv()
 
-# Quart (аналог Flask)
 app = Quart(__name__)
 
-
-# Telegram Bot токен
 TOKEN = os.getenv('TOKEN')
-USER_CHAT_ID = os.getenv('USER_CHAT_ID')  # Используйте реальный chat_id, полученный через команду /start
+GROUP_CHAT_ID = os.getenv('GROUP_CHAT_ID')
 
-# Настройка бота и диспетчера
-bot = Bot(token=os.getenv('TOKEN'))
+bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
+async def save_to_db(name, email, phone, message, tg):
+    async with aiosqlite.connect('form_data.db') as db:
+        await db.execute('''
+            INSERT INTO form_submissions (name, email, phone, message, tg)
+            VALUES (?, ?, ?, ?, ?)
+        ''', (name, email, phone, message, tg))
+        await db.commit()
 
-# Подключение к базе данных SQLite
-def save_to_db(name, email, phone, message):
-    conn = sqlite3.connect('form_data.db')
-    cursor = conn.cursor()
-
-    # Вставляем данные формы в таблицу
-    cursor.execute('''
-        INSERT INTO form_submissions (name, email, phone, message)
-        VALUES (?, ?, ?, ?)
-    ''', (name, email, phone, message))
-
-    conn.commit()
-    conn.close()
-
-
-# Асинхронная функция для отправки сообщения пользователю
 async def send_message(text):
     try:
-        await bot.send_message(chat_id=os.getenv('USER_CHAT_ID'), text=text)
+        await bot.send_message(chat_id=GROUP_CHAT_ID, text=text)
     except Exception as e:
         print(f"Ошибка при отправке сообщения: {e}")
 
-
-# Обработчик команды /start
 @dp.message_handler(commands=['start'])
 async def start_command(message: Message):
-    await message.answer("Привет! Теперь я могу отправлять тебе сообщения.")
+    await message.answer("Привет! Теперь я могу отправлять сообщения в группу.")
     print(f"Chat ID пользователя {message.from_user.username}: {message.chat.id}")
 
-
-# Quart маршрут для обработки формы
 @app.route('/submit-form', methods=['POST'])
 async def submit_form():
-    data = await request.form  # Получаем данные формы
+    data = await request.form
     name = data['name']
     email = data['email']
     phone = data['phone']
     message = data['message']
-    tg = data['tg']
+    tg = data['tg'] if 'tg' in data else ''  # Задаем значение по умолчанию, если tg отсутствует
 
-    # Сохраняем данные в базу данных
-    save_to_db(name, email, phone, message)
+    if not tg:  # Проверяем, если tg пустое, можно вернуть ошибку
+        return jsonify({"status": "error", "message": "Поле Telegram не может быть пустым!"}), 400
 
-    # Формируем текст сообщения для отправки в бота
+    await save_to_db(name, email, phone, message, tg)
+
     text = f"Получено новое сообщение с сайта:\n\n" \
            f"Имя: {name}\n" \
            f"Почта: {email}\n" \
@@ -74,33 +57,22 @@ async def submit_form():
            f"Сообщение: {message}\n" \
            f"Телеграм: {tg}"
 
-    # Отправляем сообщение в Telegram
     await send_message(text)
 
     return jsonify({"status": "success", "message": "Форма успешно отправлена!"})
 
-
-# Quart маршрут для отображения HTML
 @app.route('/')
 async def index():
     return await render_template('index.html')
 
-
-# Запуск Telegram бота
 async def run_bot():
     await dp.start_polling()
 
-
-# Основной асинхронный запуск Quart и бота
 async def main():
-    bot_task = asyncio.create_task(run_bot())  # Запускаем бота в отдельной задаче
-    quart_task = asyncio.create_task(app.run_task())  # Запускаем Quart сервер
+    bot_task = asyncio.create_task(run_bot())
+    quart_task = asyncio.create_task(app.run_task())
     await asyncio.gather(bot_task, quart_task)
 
-
 if __name__ == "__main__":
-
-    # Инициализация базы данных
     init_db()
-
     asyncio.run(main())
